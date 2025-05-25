805be9f3a7201dcde4248eb6bb101e52d1b19eef
mikaelcodes <mikael.hussain05@gmail.com>
2025-05-25 12:31:07 -0400

    added new asset

--- Assets/NewEmptyCSharpScript.cs
+++ Assets/NewEmptyCSharpScript.cs
@@ -11,6 +11,11 @@ public class NewEmptyCSharpScript : MonoBehaviour
 
         Vector3 movement = new Vector3(moveX, 0, moveY) * speed * Time.deltaTime;
         transform.Translate(movement, Space.World);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key was pressed!");
+            transform.Translate(Vector3.up * 50.0f * Time.deltaTime, Space.World);
+        }
 
 
     
